cb05eb22470649ceb7261991af921c7f3bab7a0e
regontornic <regontornic@gmail.com>
2022-09-29 14:15:15 +0900

    JS: Navbar

---
+++ main.js
@@ -0,0 +1,14 @@
+// 미친짓 못하도록 미연에 방지
+'use strict';
+
+
+// Make navbar transparent when it its on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  if(window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
