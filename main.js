1ca40c16d98fbca6f76e84c884a988685a2078a3
regontornic <regontornic@gmail.com>
2022-09-30 09:54:15 +0900

    JS: Navbar menu toggle + Project UI tweak

--- main.js
+++ main.js
@@ -21,7 +21,14 @@ navbarMenu.addEventListener('click', (event) => {
   if(link == null) {
     return;
   }
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
 
 // Handle click on "contact me" button on home
@@ -82,10 +89,7 @@ workBtnContainer.addEventListener('click', (e) => {
     });
     proejctContainer.classList.remove('anim-out');
   }, 300);
-
 });
-
-
 
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
